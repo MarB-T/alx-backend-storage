03f2a59e6465b9cc06209a2695aa7bb16b87d9ed
MarB-T <danmarbet@gmail.com>
2024-01-18 12:44:25 +0300

    create table users correction

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a table users
 -- task 0 'We are all unique
-CREATE TABLE IF NOT EXISTS users (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	email VARCHAR(255) UNIQUE NOT NULL,
-	name VARCHAR(255)
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`email` VARCHAR(255) UNIQUE NOT NULL,
+	`name` VARCHAR(255)
 );
