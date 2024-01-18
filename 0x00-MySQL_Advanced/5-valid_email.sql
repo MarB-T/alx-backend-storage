5872b418e84f1b75eb710b5a0a0f51a0ca22595a
MarB-T <danmarbet@gmail.com>
2024-01-19 00:25:08 +0300

    script that creates a trigger that resets valid_email attribute

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- SQL script to create a trigger to reset email
+-- task5 email validation sent
+DELIMITER //
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
+FOR EACH ROW
+	BEGIN
+		IF NEW.email <> OLD.email THEN
+			SET NEW.valid_email = NULL;
+		END IF;
+	END;
+//
+DELIMETER ;
