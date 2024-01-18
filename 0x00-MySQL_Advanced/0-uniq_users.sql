2841eaa251899b1e668a4a88a4cf6022a7931218
MarB-T <danmarbet@gmail.com>
2024-01-18 12:09:27 +0300

    create table users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255)
+);
