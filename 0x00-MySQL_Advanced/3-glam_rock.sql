ac9d2848f4e9d295533ec9a5d421f05392876117
MarB-T <danmarbet@gmail.com>
2024-01-18 23:02:56 +0300

    perform compute with a given default value where there is a NULL correction1

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- SQL script to list bands with 'glam rock' as their main style
 -- task3 Old school band
 SELECT DISTINCT band_name, IFNULL(split, 2022) - formed AS lifespan FROM metal_bands
-WHERE style = 'Glam rock' ORDER BY lifespan DESC;
+WHERE FIND_IN_SET('Glam rock', style) ORDER BY lifespan DESC;
